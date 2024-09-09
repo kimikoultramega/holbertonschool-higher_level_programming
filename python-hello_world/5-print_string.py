87c4aaa43154bdf70548963fba37d4ba14f891ae
kimikoultramega <9379@holbertonstudents.com>
2024-09-09 15:25:40 -0300

    hello world

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
 Holberton, School = str.split()
-print(f"{Holberton} {School}{Holberton} {School}{Holberton} {School}")
-print(f"{str[:9]}\n")
+print(f"{Holberton} {School}{Holberton} {School}{Holberton} {School}\n{str[:9]}")

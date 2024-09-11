eb271620441110a2090f48045b3e548d2e5a5972
kimikoultramega <9379@holbertonstudents.com>
2024-09-11 16:26:42 -0300

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        a = ' '.join(map(str, row))
+        a = ' '.join('{}' for _ in row).format(*row) #(map(str, row))
         print(a)

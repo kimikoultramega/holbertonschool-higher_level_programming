e3a2a416fc632f6c7bfd9310468bf6919df38f51
kimikoultramega <9379@holbertonstudents.com>
2024-09-12 20:32:19 -0300

    max value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    valor_max = my_list[0] #tomamos como el valor máximo [0]
+    valor_max = my_list[0]  # tomamos como el valor máximo [0]
     for num in my_list:
         if num > valor_max:
             valor_max = num

24899fe8faa44bb4fd59b31bbdbe2a1368485522
kimikoultramega <9379@holbertonstudents.com>
2024-09-13 16:48:53 -0300

    matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return []
     new_matrix = [[i ** 2 for i in row] for row in matrix]
-    return(new_matrix)
+    return (new_matrix)

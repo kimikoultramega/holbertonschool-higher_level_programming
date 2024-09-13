9e50fc76346f9e0b574e1b8eaf8828ece4a8e325
kimikoultramega <9379@holbertonstudents.com>
2024-09-13 19:21:05 -0300

    .append

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in my_list: # iteramos sobre my list
-        if i == search: # cuando i sea igual a search
-            new_list.append(replace) #a gregamos a new_list en el lugar i a replace
-        else: # de lo contrario agregamos a new list i
+    for i in my_list:  # iteramos sobre my list
+        if i == search:  # cuando i sea igual a search
+            new_list.append(replace)  # a gregamos a new_list en el lugar i
+        else:  # de lo contrario agregamos a new list i
             new_list.append(i)
-    return(new_list)
+    return (new_list)

d53e478cd0ac9c40be789aaaa6e22a9490270869
kimikoultramega <9379@holbertonstudents.com>
2024-09-16 13:23:12 -0300

    again

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range (97, 123) if i not in ('q', 'e')), end = "")
+for i in range(97, 123):
+    if chr(i) not in ('q', 'e'):
+        print("{:c}".format(i), end="")

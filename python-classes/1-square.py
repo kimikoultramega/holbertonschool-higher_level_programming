aab8267b5caf60575ddd9ad20c230a0c1ffdeeac
kimikoultramega <9379@holbertonstudents.com>
2024-09-17 13:05:42 -0300

    The creator game

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-
+The game creation
 """
 class Square:
     """
-
+    The class creation
     """
     def __init__(self, size):
+        """
+        The object creation
+        """
         self.__size = size

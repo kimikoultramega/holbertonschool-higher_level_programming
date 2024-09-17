b74d76c45c9428801ada046141274ed10575747d
kimikoultramega <9379@holbertonstudents.com>
2024-09-17 17:01:15 -0300

    again again and again

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,7 +18,7 @@ class Square:
         Initialize the size
         """
 
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise TypeError("size must be >= 0")

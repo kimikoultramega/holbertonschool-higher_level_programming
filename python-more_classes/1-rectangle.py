959337f9521a324fa6857bbd0d06afbeef889db8
kimikoultramega <9379@holbertonstudents.com>
2024-09-23 17:00:38 -0300

    Sherlock Holmes 2

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         """
         Documentación
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def height(self):

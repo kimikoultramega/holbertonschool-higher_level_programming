a4ca9738e815b5511c9e754bed8d80915ecbec1c
kimikoultramega <9379@holbertonstudents.com>
2024-09-25 19:14:15 -0300

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+class BaseGeometry:
+    """
+    Doc
+    """
+    def area(self):
+        """
+        Doc
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Doc
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """
+    Doc
+    """
+    def __init__(self, width, height):
+        """
+        Doc
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height

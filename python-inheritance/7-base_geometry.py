ea7a7f5c5380bd46c1e3d4070d787fc868c351a8
kimikoultramega <9379@holbertonstudents.com>
2024-09-25 17:09:22 -0300

    test

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+class BaseGeometry:
+    """
+    Doc
+    """
+    def area(self):
+        """
+        Doc
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        Doc
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

5292e44c881ab070dc5218411038dbf06003c1f6
kimikoultramega <9379@holbertonstudents.com>
2024-09-25 19:38:45 -0300

    zenwo

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-def area(self):
-    return self.__width * self.__height
+    def area(self):
+        return self.__width * self.__height
 
-def __str__(self):
-    return f"[Rectangle] {self.__width}/{self.__height}"
+    def __str__(self):
+        return "[Rectangle] {self.__width}/{self.__height}"

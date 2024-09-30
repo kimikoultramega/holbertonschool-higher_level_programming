6fd49eea919170107444775b12528df950909ec4
kimikoultramega <9379@holbertonstudents.com>
2024-09-30 20:23:53 -0300

    Roar

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class SwimMixin():
+    def swim(self):
+        print("The creature swims!")
+
+class Flymixin():
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, Flymixin)
+    def roar(self):
+        print("The dragon roars!")
+
+draco = Dragon()
+draco.swim
+draco.fly

7958e1653552de298539292314fd2985732c85b0
kimikoultramega <9379@holbertonstudents.com>
2024-10-03 19:40:26 -0300

    student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Code
+"""
+
+
+class Student():
+    """
+    Doc
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        variable = self.__dict__
+        return variable

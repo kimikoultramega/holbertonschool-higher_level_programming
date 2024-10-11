fd6b78cd071a62323c3d43bff6ef1ff4490d199b
kimikoultramega <9379@holbertonstudents.com>
2024-10-11 11:47:19 -0300

    Check one

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,12 +1,64 @@
-
+#!/usr/bin/python3
 
 import requests
 import csv
 
-response = requests.get('https://jsonplaceholder.typicode.com/posts')
+url = "https://jsonplaceholder.typicode.com/posts"
 
-print(f"Status code: {response.status_code}")
 
-if response.status_code == 200:
+def fetch_and_print_posts():
+
+    response = requests.get(url)
+
+    print(f"Status code: {response.status_code}")
+
+    if response.status_code == 200:
     # Convertimos la respuesta en una lista de diccionarios
-    posts = response.json()
+        posts = response.json()
+
+        for post in posts:
+            print(f"Title : {post['title']}")
+    else:
+        print("Error")
+
+def fetch_and_save_posts():
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        posts = response.json() # Post es una lista de diccionarios
+
+    else:
+        print("Error")
+    
+    post_to_save = [] # Lista vacía
+    
+    for post in posts: # Bucle
+
+        post_data = {
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        }
+
+        post_to_save.append(post_data)
+
+    with open('posts.csv', mode='w', newline="") as file:
+
+        # Definimos claves como encabezados
+        fieldnames = ["id", "title", "body"]
+
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        writer.writerows(post_to_save)
+
+        print("Datos guaraddos en posts.csv exitosamente")
+
+    else:
+        print("Datos guardados en posts.csv exitosamente")
+    
+    fetch_and_print_posts()
+    fetch_and_save_posts

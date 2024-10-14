4ab9c52acfaa11b2b987751b18cf0a299231e630
kimikoultramega <9379@holbertonstudents.com>
2024-10-14 16:36:54 -0300

    jhon

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,7 +6,7 @@ import json
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
-
+        
         if self.path == "/":
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -18,7 +18,7 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             response = {
-                "name": "Jhon", 
+                "name": "John", 
                 "age": 30, 
                 "city": "New York"
                 }
@@ -34,7 +34,7 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 if __name__ == "__main__":
     server_address = ("", 8000)

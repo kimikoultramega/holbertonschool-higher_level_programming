01c6d3888c5a4a2b9eeba56f5f03845cf508148b
kimikoultramega <9379@holbertonstudents.com>
2024-10-16 11:30:33 -0300

    Montaña

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,14 +1,48 @@
 #!/usr/bin/python3
 
 from flask import Flask
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
+users = {}
+
 @app.route("/")
-
 def home():
-
     return "Welcome to the Flask API!"
+
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")  # Ruta dinámica
+def get_user(username):  # El valor de username se pasa como parametro
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User  not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")  # Fix: added quotes around "username"
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    
+    users[username] = {
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+
+    return jsonify({"message": "User  added", "user": users[username]})
 
 if __name__ == "__main__":
     app.run()

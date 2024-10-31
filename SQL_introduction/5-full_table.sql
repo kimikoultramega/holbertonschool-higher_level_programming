a6153f8ea3da3ce05b989a20d6e162a057c77edd
kimikoultramega <9379@holbertonstudents.com>
2024-10-31 07:46:42 -0700

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Full description
 
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;

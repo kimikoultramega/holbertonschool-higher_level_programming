40dfbc09e2402f8dc291f45fe32f97ac5b23838c
kimikoultramega <9379@holbertonstudents.com>
2024-12-11 13:24:43 -0300

    tiempo muerto

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
 
+# def fizzbuzz():
+
+#     for i in range(1, 101):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("{}".format("FizzBuzz"), end=" ")
+#         elif i % 3 == 0:
+#             print("{}".format("Fizz"), end=" ")
+#         elif i % 5 == 0:
+#             print("{}".format("Buzz"), end=" ")
+#         else:
+#             print("{}".format(i), end=" " if i != 100 else "")
+
+
 def fizzbuzz():
 
     for i in range(1, 101):
+        str = i
         if i % 3 == 0 and i % 5 == 0:
-            print("{}".format("FizzBuzz"), end=" ")
+            str = "FizzBuzz"
         elif i % 3 == 0:
-            print("{}".format("Fizz"), end=" ")
+            str = "Fizz"
         elif i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
-        else:
-            print("{}".format(i), end=" " if i != 100 else "")
+            str = "Buzz"
+
+        print(str, end=" " if i != 100 else "")
+
+def fizzbuzz():
+    d = {3: "Fizz", 5: "Buzz", 7: "Fuzz", 9: "Bizz", 15: "FizzBuzz"}
+
+    for i in range(1, 101):
+        str = i
+        for k, v in d.items(): # [[3, "fizz"], [5, "buzz"], [15, "fizzbuzz"]]
+            if i % k == 0:
+                str = v
+    
+        print(str, end=" " if i != 100 else "")

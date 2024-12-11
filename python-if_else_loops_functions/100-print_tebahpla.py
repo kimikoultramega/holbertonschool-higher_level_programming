40a80c8cf65dd069cc4ce28061aba0b079fd6441
kimikoultramega <9379@holbertonstudents.com>
2024-12-11 13:26:00 -0300

    wakanda

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,4 @@
-#!/usr/bin/python3
 
-# def a():
-#     for i in range(ord('z'), ord('a') -1, -1):
-#         char = chr(i)
-#         if i % 2 == 0:
-#             print(f"{char.lower()}", end="")
-            
-#         elif i % 2 == 1:
-#             print(f"{char.upper()}", end="")
-            
 def b():
     for i in range(ord('z'), ord('a') -1, -1):
         char = chr(i)

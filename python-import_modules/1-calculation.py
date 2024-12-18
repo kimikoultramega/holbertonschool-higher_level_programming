2f170914157f36643d21f3da59a7f15cb64b3178
kimikoultramega <9379@holbertonstudents.com>
2024-12-18 11:51:00 -0300

    tiempo muerto 4

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add,sub,mul,div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-    result = add(a,b)
-    result2 = sub(a,b)
-    result3 = mul(a,b)
-    result4 = div(a,b)
-
-    print("{} + {} = {}".format(a, b, result))
-    print("{} + {} = {}".format(a, b, result2))
-    print("{} + {} = {}".format(a, b, result3))
-    print("{} + {} = {}".format(a, b, result4))
+    
+    print("{} + {} = {}".format(a, b, add(a, b))) # Suma
+    print("{} + {} = {}".format(a, b, sub(a,b))) # Resta
+    print("{} + {} = {}".format(a, b, mul(a,b))) # Multiplicación
+    print("{} + {} = {}".format(a, b, div(a,b))) # División

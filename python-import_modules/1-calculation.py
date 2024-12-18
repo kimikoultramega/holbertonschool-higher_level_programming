5b28c0c63f4ac1a7a7a167319e9f9704aafb6ce3
kimikoultramega <9379@holbertonstudents.com>
2024-12-18 15:36:23 -0300

    wakanda forever

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,8 +5,18 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
+
+    string = "{} {} {} = {}\n"
     
-    print("{} + {} = {}".format(a, b, add(a, b))) # Suma
-    print("{} + {} = {}".format(a, b, sub(a,b))) # Resta
-    print("{} + {} = {}".format(a, b, mul(a,b))) # Multiplicación
-    print("{} + {} = {}".format(a, b, div(a,b))) # División
+    result = (
+        string.format(a, "+", b, add(a, b)) +
+        string.format(a, "-", b, sub(a, b)) +
+        string.format(a, "*", b, mul(a, b)) +
+        string.format(a, "/", b, div(a, b))
+    )
+    print(result, end="")
+
+    # print("{} + {} = {}".format(a, b, add(a, b))) # Suma
+    # print("{} + {} = {}".format(a, b, sub(a,b))) # Resta
+    # print("{} + {} = {}".format(a, b, mul(a,b))) # Multiplicación
+    # print("{} + {} = {}".format(a, b, div(a,b))) # División

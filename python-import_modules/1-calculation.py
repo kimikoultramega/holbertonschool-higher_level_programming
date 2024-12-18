c7b98c2cc17d41ec5d6ba47c2331ad6571779704
kimikoultramega <9379@holbertonstudents.com>
2024-12-18 11:44:41 -0300

    tiempo muerto 3

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add,sub,mul,div
+
+    a = 10
+    b = 5
+    result = add(a,b)
+    result2 = sub(a,b)
+    result3 = mul(a,b)
+    result4 = div(a,b)
+
+    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result2))
+    print("{} + {} = {}".format(a, b, result3))
+    print("{} + {} = {}".format(a, b, result4))

24cff752ef26e38e4ceae47ab5eab6823e1bf37b
kimikoultramega <9379@holbertonstudents.com>
2025-01-16 10:01:44 -0300

    me costó

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# generemos el rango
+rango = range(97, 123)
+
+resultado = ""
+
+for i in rango:
+    if chr(i) not in ('q', 'e'):
+        resultado += chr(i)
+print(resultado, end="")

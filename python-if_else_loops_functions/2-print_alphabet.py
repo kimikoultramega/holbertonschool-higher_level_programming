c8ae1fb12cd386041e6d027db638c48ac35d0743
kimikoultramega <9379@holbertonstudents.com>
2025-01-16 09:18:30 -0300

    wakando linux

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-print("{}".format("".join(chr(i) for i in range(97, 123))))
+letras_en_rango = range(97, 123)
+
+letras_convertidas = (chr(i) for i in letras_en_rango)
+
+letras_unidas = "".join(letras_convertidas)
+
+print(f"{letras_unidas}")

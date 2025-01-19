69d9dafa3d86ef6e699d64ad210f5141684b9e7b
kimikoultramega <9379@holbertonstudents.com>
2025-01-19 19:00:35 -0300

    loko

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
+    if len(c) != 1 or not isinstance(c, str):
+        raise ValueError("No me descanses")
     if 'a' <= c <= 'z':
         return True
     else:

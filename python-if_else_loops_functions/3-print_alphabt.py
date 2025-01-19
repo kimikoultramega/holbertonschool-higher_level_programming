59d82d0112d3da46c1038680941a161a664f7b58
kimikoultramega <9379@holbertonstudents.com>
2025-01-19 11:29:20 -0300

    loko 44

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
-# generemos el rango
-rango = range(97, 123)
+# # generemos el rango
+# rango = range(97, 123)
 
-resultado = ""
+# resultado = ""
 
-for i in rango:
-    if chr(i) not in ('q', 'e'):
-        resultado += chr(i)
-print("{}".format(resultado), end="")
+# for i in rango:
+#     if chr(i) not in ('q', 'e'):
+#         resultado += chr(i)
+# print("{}".format(resultado), end="")
+
+secuencia = [chr(i) for i in range(97, 123)]
+
+unión = []
+
+for i in secuencia:
+    if i not in ('q', 'e'):
+        unión.append(i)
+
+result = "".join(unión)
+print(result)

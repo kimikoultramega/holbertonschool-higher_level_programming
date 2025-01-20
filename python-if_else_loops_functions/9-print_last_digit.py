e316b307944b7ce653abc8680e4c1e6b19db363a
kimikoultramega <9379@holbertonstudents.com>
2025-01-19 21:48:21 -0300

    wooo

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,12 @@
 
 def print_last_digit(number):
 
-    absoluto = abs(number % 10)
-    print("{}".format(absoluto), end="")
-    return (absoluto)
+    # absoluto = abs(number % 10)
+    # print("{}".format(absoluto), end="")
+    # return (absoluto)
+
+    numero = str(number)
+    numero = numero[-1]
+    number = int(numero)
+    print("{}".format(number), end="")
+    return number

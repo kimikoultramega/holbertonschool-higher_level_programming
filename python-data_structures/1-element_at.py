560327e1df99d0667e0fdc02b66c09eb3fe53d81
kimikoultramega <9379@holbertonstudents.com>
2025-01-21 10:12:00 -0300

    proceso del pensamiento

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    length = len(my_list)
+    if idx < 0 or idx >= length:
+        return None
+    else:
+        return my_list[idx]
+    
+    # for i in my_list:
+    #     idx = i
+    #     if i < 0 or i >= lenght:
+    #         return None

705cac6d6cec035f5f12d0dfb19b11b3f97a6476
kimikoultramega <9379@holbertonstudents.com>
2025-01-25 22:10:13 -0300

    dare devil

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
-        print("".join("{:d}".format(num) for num in fila))
+        print(" ".join("{:d}".format(num) for num in fila))

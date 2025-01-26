e34d7a399ee67dc867720b4008b9ed26bee9b8a1
kimikoultramega <9379@holbertonstudents.com>
2025-01-25 22:02:37 -0300

    wakanda

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
-        for num in fila:
-            print("{:d}".format(num), end=" ")
-        print()
+            print(" ".join("{:d}".format(num) for num in fila))

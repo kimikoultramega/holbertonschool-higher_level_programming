c3e2640ba7a15781c3a504ad8a6c2a183b986e92
kimikoultramega <9379@holbertonstudents.com>
2025-01-26 01:27:04 -0300

    ready for this

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-
-    max_value = my_list[0]
     if not my_list:
         return None
-    
     else:
-         for num in my_list:
+        max_value = my_list[0]  # Inicializa con el primer elemento
+        for num in my_list:
             if num > max_value:
-                max_value = num
-                return max_value
+                max_value = num  # Actualiza si encuentra un valor mayor
+        return max_value  # Retorna el valor máximo encontrado

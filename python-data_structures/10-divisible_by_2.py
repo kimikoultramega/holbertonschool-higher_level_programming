7bd2dfbce5226425db0106066523acb9c9ed6bbb
kimikoultramega <9379@holbertonstudents.com>
2025-01-26 15:19:46 -0300

    wakanda vibes

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    copy_list = []
+
+    if not my_list:
+        return None
+    
+    for num in my_list:
+        if num % 2 == 0:
+            copy_list.append(True)
+        else:
+            copy_list.append(False)
+    return copy_list

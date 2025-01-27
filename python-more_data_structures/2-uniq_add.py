190f765091cbda8171a0a20f23db7d50e11a7484
kimikoultramega <9379@holbertonstudents.com>
2025-01-26 21:27:34 -0300

    hey hey hey

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -5,6 +5,9 @@ def uniq_add(my_list=[]):
     suma_unicos = 0
 
     for elemento in my_list:
-        if my_list.count(elemento) == 1:
-            suma_unicos += elemento
+        if isinstance(elemento, int):
+                if my_list.count(elemento) == 1:
+                    suma_unicos += elemento
+        else:
+             pass
     return suma_unicos

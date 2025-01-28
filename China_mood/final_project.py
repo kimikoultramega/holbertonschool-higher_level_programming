8f360970e336ea80189b2e7ca138467dc356c790
kimikoultramega <9379@holbertonstudents.com>
2025-01-28 09:46:39 -0300

    Square

---
+++ China_mood/final_project.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("Saludo inicial")
+lugar_nacimiento = input("¿Dónde naciste?\n")
+mascota = input("Nombre de la mascota?\n")
+combinación = mascota + lugar_nacimiento
+print("El nombre de tu banda es: " + combinación)
---
+++ China_mood/practica.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+usarname = input("What is your name?")
+
+length = len(usarname)
+
+print(length)

6800b3cc142ba0846accfdd25f2c27ee1d39a034
kimikoultramega <9379@holbertonstudents.com>
2025-02-08 19:42:01 -0300

    un poco siempr es bueno

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+class BaseGeometry:
+    """
+    Doc
+    """
+    def area(self):
+        """
+        Lanzamiento
+        """
+        raise Exception("area() is not implemented")

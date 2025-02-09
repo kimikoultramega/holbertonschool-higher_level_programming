03ab3c33dcef450b50a58ea114914acaf07db85b
kimikoultramega <9379@holbertonstudents.com>
2025-02-09 17:57:42 -0300

    wop

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -13,3 +13,12 @@ class BaseGeometry:
         Lanzamiento
         """
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Doc
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name}must be greater than 0")

0475fcea145315584730b6f10d35157913f8d67c
kimikoultramega <9379@holbertonstudents.com>
2025-02-11 09:18:52 -0300

    wako

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     with open(filename, mode='r') as file:
         #  Dentro de este bloque leo el contenido
         contenido = file.read()
-        print(contenido)
+        print(contenido, end="")

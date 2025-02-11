a61565499a10be6cd4f2f0273e1c52c6f9a8e030
kimikoultramega <9379@holbertonstudents.com>
2025-02-11 10:18:08 -0300

    wiki

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""
+Wakanda y la rebelión de los gatos amorfos
+"""
+
+
+def from_json_string(my_str):
+    """
+    Gatos
+    """
+    var = json.loads(my_str)
+    return var

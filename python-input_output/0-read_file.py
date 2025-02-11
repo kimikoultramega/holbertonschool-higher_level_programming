2d2e1bca106090100af488b25892670cac1b164d
kimikoultramega <9379@holbertonstudents.com>
2025-02-11 09:17:08 -0300

    wakando for ever

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Documentación abstracta, flow criminal 5
+"""
+
 
 def read_file(filename=""):
+    """
+    Abriendo la caja de pandora, cuidado
+    """
     with open(filename, mode='r') as file:
         #  Dentro de este bloque leo el contenido
         contenido = file.read()

b00f9ea82e4b4fdba50b2e809c534d7e8a74b7de
kimikoultramega <9379@holbertonstudents.com>
2025-02-11 09:41:05 -0300

    dale

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Porque es mejor ser alguien, que no ser nadie. En la masa, nadie es nada.
+"""
+
 
 def write_file(filename="", text=""):
+    """
+    Abriendo, escribiendo, cerrando, retornando. Camino zen.
+    """
     with open(filename, mode='w', encoding="utf-8") as file:
         #  Dentro del bloque utilizamos write.
         escribir = file.write(text)

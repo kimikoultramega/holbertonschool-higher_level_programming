f6e9585390013ec9f6d8869f0bf2b061dcc58613
kimikoultramega <9379@holbertonstudents.com>
2025-02-16 22:29:55 -0300

    dale

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+import json
+import sys
+import os
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+def main():
+    """
+    Doc
+    """
+    filename = "add_item.json"
+
+    if os.path.exists(filename):
+        data = load_from_json_file(filename)
+    else:
+        data = []
+
+    for arg in sys.argv[1:]:
+        data.append(arg)
+
+    save_to_json_file(data, filename)

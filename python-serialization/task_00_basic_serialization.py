cbc963ef8ddeaa04fec64eff5dceb503d9a45858
kimikoultramega <9379@holbertonstudents.com>
2025-02-16 23:32:25 -0300

    9292

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,9 +10,9 @@ def serialize_and_save_to_file(data, filename):
     Doc
     """
     with open(filename, mode='w', encoding='utf-8') as file:
-        write_python_json = json.dump(data, file)
+        json.dump(data, file)
 
-def laod_and_deserialize(filename):
+def load_and_deserialize(filename):
     with open(filename, mode='r', encoding='utf-8') as file:
         data = json.load(file)
         return data
